b38173a1340abe0896bfad734889cb45b4990ddb
diego_santeri <diego.santeri@gmail.com>
2018-09-26 17:31:27 -0300

    Fixing getDependency bug

--- test/index.spec.js
+++ test/index.spec.js
@@ -16,4 +16,19 @@ describe('Testing lib dependencies load', ()=>{
             done();
         });
     })
+
+    it('should load all dependencies and subdepencies', (done)=>{
+
+        index.init('/test/src/bootstrap/dependencies.json').then(instances => {
+            expect(instances.UserModel.constructor.name).toBe('UserModel');
+            expect(instances.RoleModel.constructor.name).toBe('RoleModel');
+            expect(instances.UserRepository.constructor.name).toBe('UserRepository');
+            expect(instances.PermissionRepository.constructor.name).toBe('PermissionRepository');
+            expect(instances.UserService.constructor.name).toBe('UserService');
+            expect(instances.UserController.constructor.name).toBe('UserController');
+            expect(instances.OtherService.constructor.name).toBe('OtherService');
+
+            done();
+        });
+    })
 })
