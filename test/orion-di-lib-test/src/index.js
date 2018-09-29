b6f35477cb69f679fc9b1feafb63f0cf548cdf7d
diego <diegosanteri@gmail.com>
2018-09-29 17:07:12 -0300

    Adding test folder

---
+++ test/orion-di-lib-test/src/index.js
@@ -0,0 +1,5 @@
+module.exports = class OrionDiLibExample {
+    constructor(libService) {
+        this.libService = libService;
+    }
+}
---
+++ test/orion-di-lib-test/src/service/libService.js
@@ -0,0 +1,5 @@
+module.exports = class LibService {
+    constructor(libRepository) {
+        this.libRepository = libRepository;
+    }
+}
